48456b0d315b5b4669d915545ce6ab484457361b
Divy027 <105413340+Divy027@users.noreply.github.com>
2022-08-26 21:11:50 +0530

    Update multiples.js

--- multiples.js
+++ multiples.js
@@ -1,12 +1,13 @@
 
-    const sumOfMultiple = ( N , z) => {
-        let sum = 0;
-        for (let i = 1 ; i< z ; i++ ){
-            sum = sum + (N * i);
+   function multiples(){
+    let sum = 0;
+    for (let i = 1 ; i<= 1000; i++ ){
+        if( i %3 === 0 && i % 5 === 0){
+            console.log(i);
+            sum = sum + i;
         }
-        console.log(sum);
-    
     }
+    console.log(sum);
+   }
 
-    sumOfMultiple(3,1000) // sum of multiple of 3 under 1000
-    sumOfMultiple(5,1000) // sum of multiple of 5 under 1000
+   multiples();
